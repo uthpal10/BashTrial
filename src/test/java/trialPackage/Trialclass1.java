24bf1834764e0d972d2719f611f01712c6924094
uthpal10 <uthpal.ramesh10@gmail.com>
2024-03-07 11:41:06 +0530

    Commit the classes

---
+++ src/test/java/trialPackage/Trialclass1.java
@@ -0,0 +1,9 @@
+package trialPackage;
+
+public class Trialclass1 {
+
+	public static void main(String[] args) {
+		System.out.println("Test 1");
+	}
+
+}
---
+++ src/test/java/trialPackage/Trialclass2.java
@@ -0,0 +1,9 @@
+package trialPackage;
+
+public class Trialclass2 {
+
+	public static void main(String[] args) {
+		System.out.println("Test 2");
+	}
+
+}
